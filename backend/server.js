939ea549db5ae2ea176acc30b9a69b41a9cb0fb5
laithsurkhi2 <lalsu020@uottawa.ca>
2024-05-03 10:28:42 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 
 import express from 'express';
 import dotenv from "dotenv";
-
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+
 const app =express(); 
-const PORT= process.env.PORT || 5000;
+const PORT= process.env.PORT || 5001;
 
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+
 
 // app.get("/", (req,res)=>{
-//     //root route http://localhost:5000/
+//     //root route http://localhost:5001/
 //     res.send("Hello world!");
 // })
 
