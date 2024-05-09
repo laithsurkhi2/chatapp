23638d96bf8ac8b481a3125d98de660e0e096568
laithsurkhi2 <lalsu020@uottawa.ca>
2024-05-09 12:53:12 -0400

    final

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import path from 'path'
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
@@ -11,6 +12,8 @@ const PORT= process.env.PORT || 5001;
 
 dotenv.config();
 
+const __dirname= path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,7 +21,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req,res)=>{
 //     //root route http://localhost:5001/
