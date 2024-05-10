7ce2036386f740f436b503a25b9f8a5e56d1c32c
laithsurkhi2 <lalsu020@uottawa.ca>
2024-05-10 11:02:48 -0400

    commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,32 +1,33 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true, // Add unique constraint to ensure uniqueness
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female"],
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+}, { timestamps: true });
 
-const userSchema= new mongoose.Schema({
-    fullName:{
-        type:String,
-        required:true,
-    },
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:6
-    },
-    gender:{
-        type:String,
-        required:true,
-        enum:["male","female"]
-    },
-    profilePic:{
-        type:String,
-        default:"",
-    },
-}, {timestamps: true});
+userSchema.index({ username: 1 });
 
-const User= mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
